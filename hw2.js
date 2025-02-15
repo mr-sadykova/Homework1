d2448112d78d04737b1109d20cb67fef5b7dc8e3
mr-sadykova <sadykovaya@yandex.ru>
2025-02-15 05:09:45 +0500

    Did 5th task

--- hw2.js
+++ hw2.js
@@ -8,18 +8,23 @@ alert(a);
 // Задание 2
 
 const yearEdition = 2007;
-alert(yearEdition);
+alert('Год выпуска первого iPhone: ' + yearEdition);
 
 // Задание 3
 
 const nameOfJavaScriptCreator =  'Брэндан Эйх';
-alert(nameOfJavaScriptCreator);
+alert('Имя создателя языка JavaScript: ' + nameOfJavaScriptCreator);
 
 // Задание 4
 
 let m = 10;
 let b = 2;
-alert(m + b);
-alert(m - b);
-alert(m * b);
-alert(m / b);
+alert('Сумма = ' + (m + b));
+alert('Разность = ' + (m - b));
+alert('Произведение = ' + (m * b));
+alert('Частное = ' + (m / b));
+
+// Задание 5
+
+const result = 2 ** 5;
+alert('Результат = ' + result);
