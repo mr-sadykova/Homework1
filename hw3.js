1d38f7ac06419bea1fb90e46de405aa009a6c0dd
mr-sadykova <sadykovaya@yandex.ru>
2025-03-04 02:24:12 +0500

    Did 5th task

--- hw3.js
+++ hw3.js
@@ -33,3 +33,31 @@ let a = '2';
 let b = '3';
 alert(Number(a) + Number(b));
 
+// Задание 5
+
+let monthNumber = 9;
+switch (monthNumber) {
+    case 1:
+    case 2:
+    case 12:
+        alert("Зима");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        alert("Весна");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        alert("Лето");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        alert("Осень");
+        break;
+    default:
+        alert("Никакой");
+        break;
+}
