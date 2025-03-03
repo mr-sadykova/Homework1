99c26d4de3b9969629ebf942193b8038867b6384
mr-sadykova <sadykovaya@yandex.ru>
2025-03-04 01:15:44 +0500

    Did 3rd task

--- hw3.js
+++ hw3.js
@@ -16,3 +16,13 @@ if (c > 0 && c < 10) {
 } else {
     console.log("Неверно");
 }
+
+// Задание 3
+
+let d = 12;
+let e = 101;
+if (d > 100 || e >100) {
+    console.log("Верно");
+} else {
+    console.log("Неверно");
+}
