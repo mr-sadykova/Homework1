e08c15b4105ae656bd6721126346f4bef6cb64c8
mr-sadykova <sadykovaya@yandex.ru>
2025-03-18 21:04:22 +0500

    Did 1st task

---
+++ hw5.js
@@ -0,0 +1,13 @@
+// Задание 1
+
+function less(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+console.log(less(4, 8));
+console.log(less(6, 6));
+console.log(less(9, 1));
