20355bb8608d5eb19a8615ee4b92fcbf766b9995
mr-sadykova <sadykovaya@yandex.ru>
2025-04-06 01:08:18 +0500

    Did 3rd task

--- hw6.js
+++ hw6.js
@@ -1,19 +1,24 @@
 // Задание 1
 
-const arr = [1, 5, 4, 10, 0, 3]
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-    if (arr[i] === 10) {
+const arr1 = [1, 5, 4, 10, 0, 3]
+for (let i = 0; i < arr1.length; i++) {
+    console.log(arr1[i]);
+    if (arr1[i] === 10) {
         break;
     }
 }
 
 // Задание 2
 
-const array = [1, 5, 4, 10, 0, 3]
-for (let i = 0; i < array.length; i++) {
-    if (array[i] === 4) {
+const arr2 = [1, 5, 4, 10, 0, 3]
+for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] === 4) {
         console.log(i);
         break;
     }
 }
+
+// Задание 3
+
+const arr3 = [1, 3, 5, 10, 20]
+console.log(arr3.join(' '));
