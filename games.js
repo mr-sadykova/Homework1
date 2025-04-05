ac7221e73fbb43598ac8c60b378dea79ce0cb16f
mr-sadykova <sadykovaya@yandex.ru>
2025-04-06 00:52:37 +0500

    Did 1st task

--- games.js
+++ games.js
@@ -1,23 +1,46 @@
 // Игра "Угадай число"
 
-function guessRandomInt() {
-    const randomNumber = Math.floor(Math.random() * 100) + 1; 
-    let guess;
+// function guessRandomInt() {
+//     const randomNumber = Math.floor(Math.random() * 100) + 1; 
+//     let guess;
 
-    while (true) {
-        guess = prompt('Угадай число от 1 до 100:'); 
-        if (guess === null) {
-            alert('Игра окончена!');
-            break;
-        }
+//     while (true) {
+//         guess = prompt('Угадай число от 1 до 100:'); 
+//         if (guess === null) {
+//             alert('Игра окончена!');
+//             break;
+//         }
 
-        if (guess < randomNumber) {
-            alert('Загаданное число больше!');
-        } else if (guess > randomNumber) {
-            alert('Загаданное число меньше!');
-        } else {
-            alert('Поздравляю! Вы угадали число!');
-            break; 
-        }
-    }
-}
+//         if (guess < randomNumber) {
+//             alert('Загаданное число больше!');
+//         } else if (guess > randomNumber) {
+//             alert('Загаданное число меньше!');
+//         } else {
+//             alert('Поздравляю! Вы угадали число!');
+//             break; 
+//         }
+//     }
+// }
+
+
+// Игра "Простая арифметика"
+
+// function simpleArithmetic() {
+//     const operations = ['+', '-', '*', '/'];
+//     const operation = operations[Math.floor(Math.random() * operations.length)];
+
+//     let a = Math.floor(Math.random() * 100) + 1;
+//     let b = Math.floor(Math.random() * 100) + 1;
+
+//     if (operation === '+') {
+//         task = `Найдите сумму чисел ${a} и ${b}`;
+//     } else if (operation === '-') {
+//         task = `Найдите разность чисел ${a} и ${b}`;
+//     }
+//     else if (operation === '*') {
+//         task = `Найдите произведение чисел ${a} и ${b}`;
+//     }
+//     else (operation === '/') {
+//         task = `Найдите частное чисел ${a} и ${b}`;
+//     }
+// }
---
+++ hw6.js
@@ -0,0 +1,9 @@
+// Задание 1
+
+const arr = [1, 5, 4, 10, 0, 3]
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    if (arr[i] === 10) {
+        break;
+    }
+}
