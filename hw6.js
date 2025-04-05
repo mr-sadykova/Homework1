260b51bc2463a7e85264b573e1369558766db3a0
mr-sadykova <sadykovaya@yandex.ru>
2025-04-06 02:00:05 +0500

    Did 9th task

--- hw6.js
+++ hw6.js
@@ -1,6 +1,6 @@
 // Задание 1
 
-const arr1 = [1, 5, 4, 10, 0, 3]
+const arr1 = [1, 5, 4, 10, 0, 3];
 for (let i = 0; i < arr1.length; i++) {
     console.log(arr1[i]);
     if (arr1[i] === 10) {
@@ -10,7 +10,7 @@ for (let i = 0; i < arr1.length; i++) {
 
 // Задание 2
 
-const arr2 = [1, 5, 4, 10, 0, 3]
+const arr2 = [1, 5, 4, 10, 0, 3];
 for (let i = 0; i < arr2.length; i++) {
     if (arr2[i] === 4) {
         console.log(i);
@@ -20,7 +20,7 @@ for (let i = 0; i < arr2.length; i++) {
 
 // Задание 3
 
-const arr3 = [1, 3, 5, 10, 20]
+const arr3 = [1, 3, 5, 10, 20];
 console.log(arr3.join(' '));
 
 // Задание 4
@@ -37,13 +37,13 @@ console.log(arr4);
 
 // Задание 5
 
-const arr5 = [1, 1, 1]
+const arr5 = [1, 1, 1];
 arr5.push(2, 2, 2);
 console.log(arr5);
 
 // Задание 6
 
-const arr6 = [9, 8, 7, 'a', 6, 5]
+const arr6 = [9, 8, 7, 'a', 6, 5];
 arr6.sort();
 const filteredArr = arr6.filter(item => typeof item !== 'string');
 console.log(filteredArr);
@@ -65,3 +65,13 @@ str = str.split('');
 str.reverse();
 str = str.join('');
 console.log(str);
+
+// Задание 9
+
+const arr9 = [[1, 2, 3],[4, 5, 6]];
+const newArr = [];
+for (let i = 0; i < arr9.length; i++) {
+    newArr.push(...arr9[i]);
+}
+console.log(newArr);
+
