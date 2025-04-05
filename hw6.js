1316487d4c2dfbe02e409a61652928ea881ff7fd
mr-sadykova <sadykovaya@yandex.ru>
2025-04-06 01:20:24 +0500

    Did 4th task

--- hw6.js
+++ hw6.js
@@ -22,3 +22,15 @@ for (let i = 0; i < arr2.length; i++) {
 
 const arr3 = [1, 3, 5, 10, 20]
 console.log(arr3.join(' '));
+
+// Задание 4
+
+const arr4 = []
+for (let i = 0; i < 3; i++) {
+    const arr42 = [];
+    for (let i = 0; i < 3; i++) {
+        arr42.push(1);        
+    }
+    arr4.push(arr42);
+}
+console.log(arr4);
